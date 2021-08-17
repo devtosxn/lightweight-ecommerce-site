18260a0e16818c2819d8c3efe877486ae74906a2
Tosin Ayodele <devtosxn@gmail.com>
2021-08-17 23:36:41 +0100

    refactor files in  nav directory

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -3,19 +3,16 @@ import "./Nav.css";
 const Nav = () => {
   return (
     <nav>
-      <div className='nav__left'>Store</div>
-      <div className='nav__middle'>
-        <div className='input__wrapper'>
-          <input type='text' />
-          <i className='fas fa-search' />
+      <div className="nav__left">Store</div>
+      <div className="nav__middle">
+        <div className="input__wrapper">
+          <input type="text" />
+          <i className="fas fa-search" />
         </div>
       </div>
-      <div className='nav__right'>
-        <div className='cart__icon'>
-          <i
-            className='fa fa-shopping-cart'
-            aria-hidden='true'
-          />
+      <div className="nav__right">
+        <div className="cart__icon">
+          <i className="fa fa-shopping-cart" aria-hidden="true" />
         </div>
       </div>
     </nav>
