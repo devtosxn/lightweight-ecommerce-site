81cb7e437a386b1f3504c4fd6bc1337c0b723193
Tosin Ayodele <devtosxn@gmail.com>
2021-08-17 23:28:28 +0100

    add HomePage component

---
+++ src/screens/HomePage.js
@@ -0,0 +1,15 @@
+import products from "../data";
+import "./HomeScreen.css";
+import ProductCard from "../components/productCard";
+
+const HomePage = () => {
+  return (
+    <div className="products__wrapper">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
+
+export default HomePage;
