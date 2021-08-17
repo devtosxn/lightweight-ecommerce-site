fc2c750cca0da3a8d2070160e2a26e6f1d2c5da8
Tosin Ayodele <devtosxn@gmail.com>
2021-08-17 23:39:18 +0100

    render Nav component in App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Nav from "../src/components/nav/Nav";
 import HomePage from "./screens/HomePage";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <HomePage />
     </div>
   );
